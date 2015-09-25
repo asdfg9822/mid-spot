376e28fbc9b2a1299e2e9dc285fd28b77442e2c7
asdfg9822 <asdfg9822@naver.com>
2015-09-25 17:37:33 +0900

    [main] 2015/09/25 17:35 jonghyeok Choi
    - 슬기씨 수정사항 추가

--- main/src/main/webapp/js/app/common.js
+++ main/src/main/webapp/js/app/common.js
@@ -1,5 +1,5 @@
 define(function () {
-  window.contextRoot = 'http://192.168.1.42:9999/main';
+  window.contextRoot = 'http://local.midspot.com:9999/main';
   window.daumMapKey = 'd18801c50ea87447d8cc8a3fefac661c';
   window.FacebookAppId = '410218975833438';
 });
