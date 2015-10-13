0857bba5d2d89a8539514e33f50523fb5201ed05
bit <bit@bit-PC>
2015-10-13 21:02:44 +0900

    2015-10-13 21:00
    1. 업체등록
    	- 사이드에 버튼 추가
    	- 업체등록 스펙 수정
    	- 업체등록 폼 수정
    	- css 수정
    2. 헤더
     - 색상 변경

--- main/src/src/main/webapp/js/app/common.js
+++
@@ -1,5 +0,0 @@
-define(function () {
-  window.contextRoot = 'http://local.midspot.com:9999/main';
-  window.daumMapKey = 'd18801c50ea87447d8cc8a3fefac661c';
-  window.FacebookAppId = '410218975833438';
-}); 
--- main/src/src/main/webapp/js/app/result.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'app/common', 'app/cbpFWTabs'], function ($) {
-	return {
-		init: function () {
-			console.log("->result->init()");
-
-			new CBPFWTabs(document.getElementById('tabs'));
-		}
-	};
-});
