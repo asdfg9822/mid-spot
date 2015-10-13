ba5791d0edea41a711a4be669e53b80e5639626c
bit <bit@bit-PC>
2015-10-13 20:47:37 +0900

    2015-10-13 20:46 1. 업체등록  - 업체등록 항목 작성 중  - css 수정  - 데이터베이스 연결 예정
    2. header
     - 색상 변경

---
+++ main/src/src/main/java/net/bitacademy/java72/dao/StartDao.java
@@ -0,0 +1,24 @@
+package net.bitacademy.java72.dao;
+
+import net.bitacademy.java72.domain.Start;
+
+public interface StartDao {
+  //List<Company> list(Map<String,Object> paramMap);
+//  List<Company> list();
+//  int delete(int no);
+//  int update(Board board);
+    int insert(Start start);
+//  Board get(int no);
+//  int countAll();
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ main/src/src/main/java/net/bitacademy/java72/service/StartService.java
@@ -0,0 +1,19 @@
+package net.bitacademy.java72.service;
+
+import net.bitacademy.java72.domain.Start;
+
+public interface StartService {
+//  List<Company> list();
+//  int delete(int no);
+//  int update(Board board);
+  int insert(Start start);
+//  Board get(int no);
+//  int countAll();*/
+}
+
+
+
+
+
+
+
