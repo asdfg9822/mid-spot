ca39215b3d0933144d06f1da1e870d523476d558
bit <bit@bit-PC>
2015-10-14 21:19:59 +0900

    2015-10-14 21:19
    1. 업체 등록
     - 데이터 베이스에 값 입력 후 출력 수정

--- main/src/main/webapp/js/app/company.js
+++ main/src/main/webapp/js/app/company.js
@@ -1,65 +1,39 @@
-define([
-		'jquery',
-		'handlebars',
-		'bootstrap',
-		'jquery.ui.widget',
-		'jquery.iframe-transport',
-		'jquery.fileupload',
-		'canvas-to-blob',
-		'load-image',
-		'jquery.fileupload-process',
-		'jquery.fileupload-image',
-		'jquery.fileupload-audio',
-		'jquery.fileupload-video',
-		'jquery.fileupload-validate',
-		'app/common'
-		], function ($, handlebars) {
+define([ 'jquery', 'handlebars', 'bootstrap', 'jquery.ui.widget',
+		'jquery.iframe-transport', 'jquery.fileupload', 'canvas-to-blob',
+		'load-image', 'jquery.fileupload-process', 'jquery.fileupload-image',
+		'jquery.fileupload-audio', 'jquery.fileupload-video',
+		'jquery.fileupload-validate', 'app/common' ], function($, handlebars) {
 
-  return {
-    init: function () {
-      console.log('->company->init()');
-      
-      $('#company-main-input').click(function (event) {
-    	  event.preventDefault();
-    	  $('#content').load('html/company.html');
-      });
-      
-      $('#company-insert').click(function (event) {
-    	  event.preventDefault();
-    	  $('#content').load('html/company_main.html');
-    	  alert('업체 등록이 완료 되었습니다.');
-      });
-      		
+	return {
+		init : function() {
+			console.log('->company->init()');
 
-    }, listSpec: function() {
+			$('#company-main-input').click(function(event) {
+				event.preventDefault();
+				$('#content').load('html/company.html');
+			});
 
-		$('.categorys-select').click(function(event){
-			event.preventDefault();
-			console.log($(this).find('div').text().trim());
-			var name = $(this).find('div').text().trim();
-	    	
-	        $.getJSON(contextRoot + '/json/dest/listSpec.do?spec_nm=' + name, function(result) {
-	            var data = result.data;
-	            console.log('data :'+data);
-	            $('#specNm').val(data.spec_nm);
-	          });
-	        
-			
-		});
-    	
-        } /* listSpec() */
-  };
+			$('#company-insert').click(function(event) {
+				event.preventDefault();
+				$('#content').load('html/company_main.html');
+				alert('업체 등록이 완료 되었습니다.');
+			});
+
+			/*
+			 * $('.categorys-select').click(function(event){
+			 * event.preventDefault();
+			 * console.log($(this).find('div').text().trim()); });
+			 */
+		},
+		listSpec : function() {
+
+			$('.categorys-select').click(function(event) {
+				event.preventDefault();
+				var name = $(this).find('div').text().trim();
+				console.log('name :' + name);
+
+			});
+
+		} /* listSpec() */
+	};
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
