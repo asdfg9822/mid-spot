c46097fb872ab980cc5b1e16c654ce0f410666b4
bit <bit@bit-PC>
2015-10-16 12:23:04 +0900

    2015-10-16 12:21 1. 업체등록  - 업체별 상세 스펙 등록 가능  - 예외 처리  - 쿼리문 수정  - UI 수정  - CSS 수정

--- main/src/main/webapp/js/app/company.js
+++ main/src/main/webapp/js/app/company.js
@@ -16,79 +16,70 @@ define([ 'jquery', 'handlebars', 'bootstrap', 'jquery.ui.widget',
 			$('#company-insert').click(function(event) {
 				event.preventDefault();
 				$('#content').load('html/company_main.html');
-				alert('업체 등록이 완료 되었습니다.');
 			});
 
 		},
 		listSpec : function() {
-
 			$('.categorys-select').click(
-				function(event) {
-					event.preventDefault();
-					console.log($(this).find('div').text().trim());
-					var name = $(this).find('div').text().trim();
+					function(event) {
+						event.preventDefault();
+						var name = $(this).find('div').text().trim();
 
-					$.getJSON(contextRoot + '/json/dest/list.do?cate_nm='+name
-							, function(result) {
-						// handlebars 라이브러리를 이용하여 테이블 tr 태그 생성
-						var source = $('#spec-template').html();
-						var template = handlebars.compile(source);
-						var content = template(result);
-						$("#content-placeholder").html(content);
-						
-						console.log(result);
+						$.getJSON(contextRoot + '/json/dest/list.do?cate_nm='
+								+ name, function(result) {
+							var source = $('#spec-template').html();
+							var template = handlebars.compile(source);
+							var content = template(result);
+							$("#content-placeholder").html(content);
+						});
 					});
-				});
 
 		},/* listSpec() */
-		
-		insertSpec: function() {
+
+		insertSpec : function() {
 			var moduleObj = this;
-			
-			$('#company-spec-insert').click(function(event){
-				var data = {};
-				$('#content-placeholder tr').each(function() {
-					data[$(this).find('th').text()] = $(this).find('input[type=text]').val();
-				});
-				
-				var jsonStr = JSON.stringify(data);
-				console.log(jsonStr);
-				
-				
-				$.getJSON(contextRoot + '/json/dest/insertSpec.do', {
-					method : 'POST',
-					dataType : 'json',
-					data : {
-						spec_nm : $(this).find('th').text(),
-						cate_no : $('#meetDate').val(),
-						spec_vl : $(this).find('input[type=text]').val(),
-						comp_nm : 
 
-					},
-					success : function(result) {
-						if (result.data == 'success') {
-							alert('입력 성공입니다.');
-						} else {
-							alert('입력할 수 없습니다.');
-						}
-					}
+			$('#company-spec-insert').click(
+					function(event) {
+						var specNmList = new Array();
+						var cateNoList = new Array();
+						var specVlList = new Array();
+						var compNmList = new Array();
 
-				});
-				
-			});
-			
-			
+						$('#content-placeholder tr').each(
+								function() {
+									specNmList.push($(this).find('th').text());
+									specVlList.push($(this).find(
+											'input[type=text]').val());
+									cateNoList.push($(this).find('th').attr(
+											'cate_no'));
+								});
+
+						for (index = 0; index < specNmList.length; index++) {
+							compNmList.push($('#compNm').find(
+							'input[type=text]').val());
+							
+							if (specVlList[index] == '') {
+								specVlList[index] = '알수 없음';
+							}
+							
+							$.ajax(contextRoot + '/json/dest/insertSpec.do', {
+								method : 'POST',
+								dataType : 'json',
+								data : {
+									spec_nm : specNmList[index],
+									cate_no : cateNoList[index],
+									spec_vl : specVlList[index],
+									comp_nm : compNmList[index]
+								}
+							});
+							
+						} /* for 문 종료 */
+						
+						
+					});
+
 		}
 
 	};
 });
-
-
-
-
-
-
-
-
-
-
